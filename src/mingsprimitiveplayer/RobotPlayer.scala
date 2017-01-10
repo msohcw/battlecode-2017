81728755a41a0072c9086de4e1907ecdd100c2e1
ming li <mingyli34@gmail.com>
2017-01-09 23:42:21 -0800

    started first robot player in scala. need to learn scala brb

---
+++ src/mingsprimitiveplayer/RobotPlayer.scala
@@ -0,0 +1,13 @@
+package mingsprimitiveplayer
+
+import battlecode.common._
+
+/**
+  * Created by mingyli on 1/9/17.
+  */
+class RobotPlayer {
+
+    def run(rc: RobotController): Unit = {
+
+    }
+}
